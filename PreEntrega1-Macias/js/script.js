6aaf9e61bed8898103410c77111cdbd50d37d632
jmacias14 <javiermacias14@hotmail.com>
2022-10-25 00:26:13 -0300

    avance2

--- PreEntrega1-Macias/js/script.js
+++ PreEntrega1-Macias/js/script.js
@@ -1,4 +1,19 @@
-let impuestosBase = 0.65;
+let impuestosBase = 1.65;
+let precioSinImp = 0;
 
-function calcularImpuestos;
-    
+
+function calcularImpuestos() {
+    let precioSinImp = document.forms["calculadoraPrecio"]["precioJuego"].value;
+    let precioFinal = precioSinImp * impuestosBase
+    console.log(precioFinal);
+    document.getElementById("precioFinal").innerHTML = '$' + (precioFinal) + '';
+    document.getElementById("campoPrecio").style.display = "block";
+
+
+}
+
+let botonCalcular = document.getElementById("botonCalcular");
+botonCalcular.addEventListener('click', event => {
+    calcularImpuestos();
+});
+
