c95c8c73a93cc7409efe0c112cda61745f027d3c
jmacias14 <javiermacias14@hotmail.com>
2022-11-07 23:39:27 -0300

    Arreglos varios

--- PreEntrega1-Macias/js/script.js
+++ PreEntrega1-Macias/js/script.js
@@ -1,16 +1,25 @@
-let impuestosBase = 1.65;
+let impuestosBase = 1.75; // Impuestos estandar de Argentina %75
 let precioSinImp = 0;
-
+let impuestoMasDe300 = 1.25; // Si gastastte mas de 300USD %25
 
 function calcularImpuestos() {
     let precioSinImp = document.forms["calculadoraPrecio"]["precioJuego"].value;
-    let precioFinal = precioSinImp * impuestosBase
-    console.log(precioFinal);
-    document.getElementById("precioFinal").innerHTML = '$' + (precioFinal) + '';
+    let precioBase = (precioSinImp * impuestosBase).toFixed(2);
+    console.log(precioBase);
+
+    var checkMasDe300 = document.getElementById('checkMasDe300');
+    if(checkMasDe300.checked){
+        var precioFinal = (precioBase * impuestoMasDe300).toFixed(2);
+    } else {
+        var precioFinal = (precioBase);
+    }
+    document.getElementById("campoPrecioFinal").innerHTML = '$' + (precioFinal) + '';
     document.getElementById("campoPrecio").style.display = "block";
 
-
+    
 }
+
+
 
 let botonCalcular = document.getElementById("botonCalcular");
 botonCalcular.addEventListener('click', event => {
